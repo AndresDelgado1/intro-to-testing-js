27a2e7da29b80ba20f02266643c474343f3e9034
Andres Delgado <andresdelgado335@gmail.com>
2022-09-15 16:45:51 -0500

    Completed exercises 6 and 7

--- code.js
+++ code.js
@@ -5,12 +5,7 @@ function helloWorld() {
 
 
 function sayHello(input) {
-    if(input === "Pat"){
-       return "Hello, Pat!"
-    } else if(input === "Alex"){
-        return "Hello, Alex!"
-    } else {
-        return "Hello, Jane!"
-    }
+    return "Hello, " + input +"!";
 }
-sayHello("Alex")
+
+console.log(sayHello());
